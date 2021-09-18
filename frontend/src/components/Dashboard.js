6638d42512197806dfef66b62114e6c3062bf981
Alexander Yoshino <alexander.yoshino@mail.utoronto.ca>
2021-09-18 14:47:44 -0400

    added file uploading

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
 import SubmitForm from './SubmitForm'
 
-const Dashboard = () => {
 
-	const handleSubmit = () => {
-		// ADD YOUR CODE HERE ALEX
+const Dashboard = () => {
+	const [fileUploaded, setFileUploaded] = useState(0);
+	const [file, setFile] = useState(0);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(file)
+		const data = new FormData();
+		data.append('file', file);
+		axios.post('http://localhost:3000/api/upload', data)
+		.then((result) => console.log(result))
+		.catch((result) => console.log(result))
+		setFileUploaded(1);
 	}
+	
 
 	return (
-		<>
-			<SubmitForm handleSubmit={handleSubmit}/>
-		</>
+			fileUploaded ? <p>Uploaded</p> : <SubmitForm handleSubmit={handleSubmit} setFile={setFile}/>
 	)
 }
 
--- frontend/src/components/SubmitForm.js
+++ frontend/src/components/SubmitForm.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "../styles/submitform.css"
 
-const SubmitForm = ({handleSubmit}) => {
+const SubmitForm = ({handleSubmit, setFile}) => {
+
+	const changeHandler = (event) => {
+		setFile(event.target.files[0]);
+	}
+
 	return (
-		<form onSubmit={() => handleSubmit()}>
-			<div className="fileUpload btn btn-outline-primary">
+		<form onSubmit={handleSubmit}>
+			<div type="button" className="fileUpload btn btn-outline-primary">
 				<span>Upload</span>
-				<input type="file" className="upload" />
+				<input type="file" name="file" onChange={changeHandler} className="upload" />
 			</div>
 			<input className="btn btn-rounded btn-outline-primary" type="submit" value="Get my Score" />
 		</form>
