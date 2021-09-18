34e693865d7aae5bdef0b94260b3314b00a33096
Alexander Yoshino <alexander.yoshino@mail.utoronto.ca>
2021-09-18 02:57:01 -0400

    initial backend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,27 @@
 from database import repo
 from flask import Flask
+from flask import request
+import authentication
 
-db = repo("test")
+db_user = repo("user_db")
 mongo_id = "614555ee4118c7e524b4948b"
-resp = db.read(mongo_id)
-print(resp)
 
 app = Flask("app")
 
-@app.route("/")
-def hello_world():
-    return "<p>Hi</p>"
+@app.route("/api/login", methods=["POST"])
+def login():
+    if "username" in request.form and "password" in request.form:
+        username, password = request.form["username"], request.form["password"]
+        auth_val = db_user.authenticate(username, password)
+        if auth_val:
+            token_val = authentication.get_token(username)
+            response = make_response()
+
+
+
+
+
+
+@app.route("/api/upload", methods=["POST"])
+def upload_api():
+    if 'file' not in request.files:
