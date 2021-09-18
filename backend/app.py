e2ad9147b6ef64c626f949a633281dc719b2f92d
Alexander Yoshino <alexander.yoshino@mail.utoronto.ca>
2021-09-17 23:59:56 -0400

    ssl cert

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from database import repo
+from flask import Flask
 
 db = repo("test")
 mongo_id = "614555ee4118c7e524b4948b"
--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,8 @@ from bson.objectid import ObjectId
 
 class repo:
     def __init__(self, collection):
-        url = "mongodb+srv://simon:simon@hackthenorth2021.kat9o.mongodb.net/test"
-        self.db = MongoClient(url)['hack'][collection]
+        url = "mongodb+srv://simon:simon@hackthenorth2021.kat9o.mongodb.net/test?ssl=true&ssl_cert_reqs=CERT_NONE"
+        self.db = MongoClient(url, connect=False)['hack'][collection]
 
     def read(self, id):
         resp = self.db.find_one({"_id": ObjectId(id)})
